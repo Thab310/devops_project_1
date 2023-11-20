f8793a9b7d383a6a544622ac41f7daa7c22fcb4c
Thabelo Ramabulana <th9b310@gmail.com>
2023-11-20 20:26:49 +0200

    Updated ansible playbook

--- terraform/5-jenkins-slave-ec2.tf
+++ terraform/5-jenkins-slave-ec2.tf
@@ -6,8 +6,10 @@ resource "aws_instance" "jenkins_slave" {
   security_groups      = [aws_security_group.jenkins_slave_sg.id]
   key_name             = local.key_name
   iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
-
+  
   tags = {
     Name = "jenkins-slave"
   }
+
+  depends_on = [ aws_iam_instance_profile.ec2_instance_profile ]
 }
